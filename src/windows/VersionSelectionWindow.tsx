15a4c80679fa30721e1ef8949e84078b0806c11e
Kurasava <flashbladepoint@mail.ru>
2025-05-02 00:39:21 +1100

    Добавлена скелетная страница для VersionSelectionWindow.tsx

--- src/windows/VersionSelectionWindow.tsx
+++ src/windows/VersionSelectionWindow.tsx
@@ -7,6 +7,7 @@ import Navigation from "../elements/Navigation.tsx";
 import ButtonNext from "../buttons/ButtonNext.tsx";
 import * as React from "react";
 import {Tooltip} from "../elements/Tooltip.tsx";
+import SkeletonButtonNext from "../buttons/SkeletonButtonNext.tsx";
 
 export default function VersionSelectionWindow(
     {activeStep, setActiveStep, minecraftVersion, setMinecraftVersion}:
@@ -38,38 +39,52 @@ export default function VersionSelectionWindow(
     }, []);
 
     function ChooseVersionMenu() {
-        if (loaded) {
-            return (
-                <div className="relative flex flex-col gap-2 w-[174px]">
-                    <div className="gap-2">
-                      <label className="text-sm font-normal text-white">
-                        Версия Minecraft
-                      </label>
-                    </div>
-                    <label htmlFor="dropdown-toggle" className="flex w-[174px] cursor-pointer items-center justify-between rounded-lg border border-gray-600 bg-gray-700 p-1 text-sm font-normal text-white">
-                        <input type="checkbox" id="dropdown-toggle" className="hidden" checked={dropMenu} onChange={() => setDropMenu(!dropMenu)}/>
-                        <span className="p-2 align-middle font-[Inter] text-sm leading-tight font-normal text-white">
-                          {minecraftVersion}
-                        </span>
-                        <ChevronDown className="text-gray-400"/>
-                    </label>
-                    <nav className={`transition-opacity duration-100 ease-out ${dropMenu ? "opacity-100" : "pointer-events-none opacity-0"} absolute top-[83px] w-[174px] flex-col items-start rounded-lg bg-gray-700 shadow-md`}>
-                        <div className="flex flex-col gap-4 p-4">
-                            {minecraftVersions.map(version => (
-                                <button key={version.version} className="flex cursor-pointer flex-row gap-2 text-sm leading-none font-normal text-white" onClick={() => {
-                                    setMinecraftVersion(version.version)
-                                    setDropMenu(!dropMenu);
-                                }}>
-                                    <StatusBadge version={version}/>
-                                    {version.version}
-                                </button>
-                            ))}
-                        </div>
-                    </nav>
+        return (
+            <div className="relative flex flex-col gap-2 w-[174px]">
+                <div className="gap-2">
+                  <label className="text-sm font-normal text-white">
+                    Версия Minecraft
+                  </label>
                 </div>
-            );
-        }
+                <label htmlFor="dropdown-toggle" className="flex w-[174px] h-[43px] cursor-pointer items-center justify-between rounded-lg border border-gray-600 bg-gray-700 p-1 text-sm font-normal text-white">
+                    <input type="checkbox" id="dropdown-toggle" className="hidden" checked={dropMenu} onChange={() => setDropMenu(!dropMenu)}/>
+                    <span className="p-2 align-middle font-[Inter] text-sm leading-tight font-normal text-white">
+                      {minecraftVersion}
+                    </span>
+                    <ChevronDown className="text-gray-400"/>
+                </label>
+                <nav className={`transition-opacity duration-100 ease-out ${dropMenu ? "flex" : "pointer-events-none hidden"} absolute top-[83px] w-[174px] flex-col items-start rounded-lg bg-gray-700 shadow-md`}>
+                    <div className="flex flex-col gap-4 p-4">
+                        {minecraftVersions.map(version => (
+                            <button key={version.version} className="flex cursor-pointer flex-row gap-2 text-sm leading-none font-normal text-white" onClick={() => {
+                                setMinecraftVersion(version.version)
+                                setDropMenu(!dropMenu);
+                            }}>
+                                <StatusBadge version={version}/>
+                                {version.version}
+                            </button>
+                        ))}
+                    </div>
+                </nav>
+            </div>
+        );
     }
+
+  function SkeletonChooseVersionMenu() {
+    return (
+      <div className="relative flex flex-col gap-2 w-[174px]">
+        <div className="gap-2">
+          <label className="text-sm font-normal text-white">
+            Версия Minecraft
+          </label>
+        </div>
+        <div className="flex w-[174px] h-[43px] cursor-pointer items-center justify-between rounded-lg border border-gray-600 bg-gray-700 p-1 text-sm font-normal text-white">
+          <div className="w-12 p-2 h-3 ml-2 bg-gray-500 rounded-md" />
+          <ChevronDown className="text-gray-400"/>
+        </div>
+      </div>
+    );
+  }
 
     function StatusBadge({ version } : { version: MinecraftVersion }) {
         return version.experimental ? (
@@ -108,19 +123,36 @@ export default function VersionSelectionWindow(
         }
     }
 
-    return (
+    if (loaded) {
+      return (
         <main className="flex h-screen w-screen items-center justify-center bg-gray-900 font-inter select-none">
-            <div className="flex w-[720px] flex-col gap-6 rounded-lg bg-gray-800 p-8 shadow">
-                <Logo />
-                <hr className="border-transparent bg-gray-700" />
-                <Navigation activeStep={activeStep} setActiveStep={setActiveStep} download={false}/>
-                <span style={{ fontSize: 17 }} className="text-base font-normal text-gray-400">
+          <div className="flex w-[720px] flex-col gap-6 rounded-lg bg-gray-800 p-8 shadow">
+            <Logo />
+            <hr className="border-transparent bg-gray-700" />
+            <Navigation activeStep={activeStep} setActiveStep={setActiveStep} download={false}/>
+            <span style={{ fontSize: 17 }} className="text-base font-normal text-gray-400">
                     Выберите версию Minecraft Java Edition, для которой нужно установить моды. Рекомендуем выбирать последнюю — для неё модпак регулярно обновляется
                 </span>
-                <ChooseVersionMenu />
-                <ButtonNext activeStep={activeStep} setActiveStep={setActiveStep} loaded={true}/>
-            </div>
+            <ChooseVersionMenu />
+            <ButtonNext activeStep={activeStep} setActiveStep={setActiveStep} loaded={loaded}/>
+          </div>
         </main>
-    );
+      )
+    } else {
+      return (
+        <main className="flex h-screen w-screen items-center justify-center bg-gray-900 font-inter select-none">
+          <div className="flex w-[720px] flex-col gap-6 rounded-lg bg-gray-800 p-8 shadow">
+            <Logo />
+            <hr className="border-transparent bg-gray-700" />
+            <Navigation activeStep={activeStep} setActiveStep={setActiveStep} download={false}/>
+            <span style={{ fontSize: 17 }} className="text-base font-normal text-gray-400">
+                    Выберите версию Minecraft Java Edition, для которой нужно установить моды. Рекомендуем выбирать последнюю — для неё модпак регулярно обновляется
+                </span>
+            <SkeletonChooseVersionMenu />
+            <SkeletonButtonNext loaded={loaded}/>
+          </div>
+        </main>
+      )
+    }
 }
 
